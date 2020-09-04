fcf808b76003387203c1ed837144d7f745f0b591
Dunc1995 <dpbailey1995@outlook.com>
2020-09-04 10:26:08 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    # Application name:
+    name="Python_Developer_Coding_Challenge",
+
+    # Version number (initial):
+    version="0.1.0",
+
+    # Application author details:
+    author="Duncan Bailey",
+    author_email="duncanbailey1995@gmail.com",
+
+    # Packages
+    packages=find_packages(),
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    # Details
+    url="https://github.com/Dunc1995/python_developer_coding_challenge",
+
+    #
+    license="LICENSE",
+    description="Coding challenge for a Python Developer job application.",
+    #long_description=open("README.txt").read(),
+
+    # Dependent packages (distributions)
+    install_requires=[ ],
+
+    entry_points={
+    'console_scripts': [
+        'damagecalc = damagecalc.__main__:main'
+    ]
+    }
+)
