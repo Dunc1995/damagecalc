50afbe248e885031a9be1eb4f1624f764d9e5b51
Dunc1995 <dpbailey1995@outlook.com>
2020-09-04 10:42:40 +0100

    adds value_in_range method to __main__.py

--- damagecalc/__main__.py
+++ damagecalc/__main__.py
@@ -13,5 +13,16 @@ def main():
 #         file.write(contents)
 #         file.close()
 
+def value_in_range(lower_limit: int, upper_limit: int, depth):
+    '''Tests whether a given depth value is within the specified range'''
+
+    if lower_limit > upper_limit:
+        raise Exception('Error at value_in_range - your lower limit cannot be greater than your upper limit!')
+    in_range = lower_limit < depth < upper_limit
+    
+    return in_range
+
+
 if __name__ == "__main__":
     main()
+    
